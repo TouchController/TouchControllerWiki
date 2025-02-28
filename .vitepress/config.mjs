7ea7483c9ccfdc3b302c986d9abaf1fd59021ea7
WhiteElephant-abc <119278153+WhiteElephant-abc@users.noreply.github.com>
2025-02-28 23:41:09 +0800

    使导航栏中的组可以折叠

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
         sidebar: [
             {
                 text: "玩家手册",
+                collapsed: false,
                 items: [
                     { text: "游玩须知", link: "/玩家手册/游玩须知" },
                     { text: "常见问题", link: "/玩家手册/常见问题" },
@@ -20,12 +21,14 @@ export default defineConfig({
             },
             {
                 text: "机制",
+                collapsed: false,
                 items: [
                     { text: "锚点机制", link: "/机制/锚点机制" },
                 ]
             },
             {
                 text: "特性",
+                collapsed: false,
                 items: [
                     { text: "触控圈", link: "/特性/触控圈" },
                     { text: "自定义布局", link: "/特性/自定义布局" },
@@ -33,17 +36,21 @@ export default defineConfig({
             },
             {
                 text: "GUI",
+                collapsed: false,
                 items: [
                     {
                         text: "设置界面",
+                        collapsed: true,
                         items: [
                             { text: "界面框架", link: "/GUI/设置界面/界面框架" },
                             {
                                 text: "标签页",
+                                collapsed: false,
                                 items: [
                                     { text: "关于", link: "/GUI/设置界面/标签页/关于" },
                                     {
                                         text: "通用",
+                                        collapsed: true,
                                         items: [
                                             { text: "常规", link: "/GUI/设置界面/标签页/通用/常规" },
                                             { text: "控制", link: "/GUI/设置界面/标签页/通用/控制" },
@@ -53,6 +60,7 @@ export default defineConfig({
                                     },
                                     {
                                         text: "物品",
+                                        collapsed: true,
                                         items: [
                                             { text: "可长按使用的物品", link: "/GUI/设置界面/标签页/物品/可长按使用的物品" },
                                             { text: "手持时显示准星的物品", link: "/GUI/设置界面/标签页/物品/手持时显示准星的物品" },
@@ -63,9 +71,11 @@ export default defineConfig({
                             },
                             {
                                 text: "子页面",
+                                collapsed: false,
                                 items: [
                                     {
                                         text: "布局",
+                                        collapsed: true,
                                         items: [
                                             { text: "管理控件预设", link: "/GUI/设置界面/子页面/布局/管理控件预设" },
                                             { text: "自定义控件布局", link: "/GUI/设置界面/子页面/布局/自定义控件布局" },
@@ -82,6 +92,7 @@ export default defineConfig({
             },
             {
                 text: "控件",
+                collapsed: false,
                 items: [
                     { text: "控件样式", link: "/控件/控件样式" },
                     { text: "游戏内控件", link: "/控件/游戏内控件" },
